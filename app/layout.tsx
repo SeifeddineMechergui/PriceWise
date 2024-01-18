b0243b079618ee627288c3908a493c0828f2c538
SeifeddineMechergui <saifmechergui2222@gmail.com>
2024-01-18 12:41:04 +0100

    initialing Nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Inter,Space_Grotesk } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+const SpaceGrotesk = Space_Grotesk({
+  subsets:['latin'],
+  weight:["300","400","500","600","700"]
+})
+
+export const metadata: Metadata = {
+  title: 'Pricewise',
+  description: 'Tack products prices effertlessly and save money on your online shoping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className='max-w-10xl mx-auto'>
+          <Navbar/>
+
+           {children}
+        </main>
+        
+        
+        </body>
+    </html>
+  )
+}
